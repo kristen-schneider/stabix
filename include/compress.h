c7c20d8c2eac23d7e3c8eeca03039835f3b8a7ea
kristen-schneider <kristen.schneider@colorado.edu>
2024-04-22 13:34:03 -0600

    updating headers for compression and decompression

--- include/compress.h
+++ include/compress.h
@@ -14,4 +14,5 @@ using namespace std;
 
 vector<string> compress_block(vector<vector<string>> block, vector<string> codecs_list);
 string zlib_compress(string in_data);
-uint32_t* fastpfor_vb_compress(vector<uint32_t> in_data);
+uint32_t* fastpfor_vb_compress(vector<uint32_t> in_data, size_t& compressedSize);
+vector<uint32_t> fastpfor_vb_delta_compress(vector<uint32_t> in_data, size_t& compressedSize);
