2141f1dbaa950c0a6ee8b3618303e416ee347c1a
Simon Walker <simon@simonwalker.tech>
2024-06-10 11:48:28 -0600

    nix flake experimentation

--- flake.nix
+++ flake.nix
@@ -1,13 +1,11 @@
 {
-  # description = "A simple flake for direnv";
-
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
   };
 
   outputs = { self, nixpkgs }:
     let
-      system = "x86_64-linux";
+      system = "aarch64-darwin";
       pkgs = nixpkgs.legacyPackages.${system};
     in
     with pkgs;
@@ -15,8 +13,12 @@
       devShells.${system}.default = mkShell {
         buildInputs = [
           # Add your desired packages here
-          nodejs
-          python3
+          zlib
+          libzip
+          gcc
+          cmake
+          simde
+          bzip2
         ];
       };
     };
