c591f8ebb4f6eece246d58f44163889157c84cef
Simon Walker <simon@simonwalker.tech>
2024-06-10 11:41:23 -0600

    nix flake experimentation

--- flake.nix
+++ flake.nix
@@ -1,40 +1,22 @@
 {
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-22.11";
+  # description = "A simple flake for direnv";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
+  };
 
   outputs = { self, nixpkgs }:
     let
-      supportedSystems = [ "x86_64-darwin" ];
-      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
-      nixpkgsFor = forAllSystems (system: import nixpkgs { inherit system; });
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
     in
     {
-      packages = forAllSystems (system:
-        let
-          pkgs = nixpkgsFor.${system};
-        in
-        {
-          default = pkgs.buildEnv {
-            name = "cmake-gcc-env";
-            paths = [
-              pkgs.cmake_3_24_3
-              pkgs.gcc11
-            ];
-          };
-        }
-      );
-
-      devShells = forAllSystems (system:
-        let
-          pkgs = nixpkgsFor.${system};
-        in
-        {
-          default = pkgs.mkShell {
-            buildInputs = [
-              pkgs.cmake_3_24_3
-              pkgs.gcc11
-            ];
-          };
-        }
-      );
+      devShells.${system}.default = pkgs.mkShell with pkgs {
+        buildInputs = [
+          # Add your desired packages here
+          nodejs
+          python3
+        ];
+      };
     };
 }
