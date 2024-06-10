100346b837d446f0fbef8c37592d7cf9c9c23f6a
Simon Walker <simon@simonwalker.tech>
2024-06-10 11:43:08 -0600

    nix flake experimentation

--- flake.nix
+++ flake.nix
@@ -10,8 +10,9 @@
       system = "x86_64-linux";
       pkgs = nixpkgs.legacyPackages.${system};
     in
+    with pkgs;
     {
-      devShells.${system}.default = pkgs.mkShell with pkgs {
+      devShells.${system}.default = mkShell {
         buildInputs = [
           # Add your desired packages here
           nodejs
