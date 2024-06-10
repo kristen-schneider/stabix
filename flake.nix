b3be348cb3769770170d079e529c29d83fafc9a7
Simon Walker <simon@simonwalker.tech>
2024-06-10 11:27:03 -0600

    added nix flake

---
+++ flake.nix
@@ -0,0 +1,22 @@
+{
+  description = "A simple flake for direnv";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in
+    {
+      devShells.${system}.default = pkgs.mkShell {
+        buildInputs = [
+          # Add your desired packages here
+          pkgs.nodejs
+          pkgs.python3
+        ];
+      };
+    };
+}
