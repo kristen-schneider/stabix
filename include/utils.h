648e6d2c7074f3b2a10d47f0f3c0222e7cb51ed8
Simon Walker <simon@simonwalker.tech>
2024-06-25 01:07:17 -0600

    improved fn docs for utils
    1. documentation usually goes in the header file
    2. added some missing fn docs
    3. cleaned up a few fn docs

--- include/utils.h
+++ include/utils.h
@@ -1,67 +1,148 @@
 #ifndef GWAS_COMPRESS_CPP_UTILS_H
 #define GWAS_COMPRESS_CPP_UTILS_H
 
-#endif //GWAS_COMPRESS_CPP_UTILS_H
+#endif // GWAS_COMPRESS_CPP_UTILS_H
 
+#include <fstream>
 #include <iostream>
+#include <map>
 #include <string>
 #include <vector>
-#include <fstream>
-#include <map>
 
 using namespace std;
 
-map<string, string> read_config_file(
-        string config_file);
+/*
+ * Read config file
+ * @param config_file
+ * @return vector of config options
+ */
+map<string, string> read_config_file(string config_file);
 
-void add_default_config_options(
-        map<string, string> &config_options);
+/*
+ * Add default config options if not present
+ * @param config_options
+ * @return void
+ */
+void add_default_config_options(map<string, string> &config_options);
 
-int get_index(
-        vector<string> vec, string str);
+template <typename T>
+bool in_array(const T &value, const std::vector<T> &array);
 
-char get_delimiter(
-        string line);
+// Python equivalent:  `return value in array`.
+// For std::vector
+template <typename T>
+bool in_array(const T &value, const std::vector<T> &array);
 
-string get_data_types(
-        string line,
-        char delimiter);
+// Python equivalent:  `return value in array`.
+// For C-style arrays
+template <typename T, size_t N>
+bool in_array(const T &value, const T (&array)[N]);
 
-vector<string> split_string(
-        string str, char delimiter);
+/*
+ * Get index value of a string in a vector
+ * @param vec: vector in which to look
+ * @param str: string to search for
+ * @return index: index value of string in vector
+ */
+int get_index(vector<string> vec, string str);
 
-string convert_vector_str_to_string(
-        vector<string> vec);
+/*
+ * Get the column types of a file
+ * @param line: string
+ * @param delimiter: char to split by
+ * @return column_types_str: string of comma separated data types
+ */
+string get_data_types(string line, char delimiter);
 
+/*
+ * Get the delimiter of a file
+ * @param line: string of first line of file
+ * @return delimiter: char to split by
+ */
+char get_delimiter(string line);
 
-string convert_vector_int_to_string(
-        vector<int> vec);
+/*
+ * Convert vector string to string
+ * @param vec: vector of strings
+ * @return str: string of comma separated values
+ */
+string convert_vector_str_to_string(vector<string> vec);
 
-vector<string> convert_string_to_vector_string(
-        string str);
+/*
+ * Convert vector of ints to string
+ * @param vec: vector of integers
+ * @return str: string of comma separated values
+ */
+string convert_vector_int_to_string(vector<int> vec);
 
-vector<uint32_t> convert_string_to_vector_unsignedlong(
-        string str);
+/*
+ * Convert string to vector
+ * @param str: string of comma separated values
+ * @return vector of strings
+ */
+vector<string> convert_string_to_vector_string(string str);
 
-vector<uint32_t> convert_vector_string_to_vector_int(
-        vector<string> vec);
+/*
+ * Convert string to vector
+ * @param str: string of comma separated values
+ * @return vector of int
+ */
+vector<uint32_t> convert_string_to_vector_unsignedlong(string str);
 
-string convert_vector_uint32_to_string(
-        uint32_t * compressed_arr,
-        size_t compressedSize);
+/*
+ * Convert a vector of strings to vector of integers
+ * @param vec: vector of strings
+ * @return vector of integers
+ */
+vector<uint32_t> convert_vector_string_to_vector_int(vector<string> vec);
 
-uint32_t * convert_string_to_vector_uint32(
-        string in_string,
-        char delimiter);
+/* Convert vector of uint32_t to string
+ * @param compressed_arr: uint32_t array
+ * @param compressed_size: size of compressed array
+ * @return compressed_string: string of comma separated values
+ */
+string convert_vector_uint32_to_string(uint32_t *compressed_arr,
+                                       size_t compressedSize);
 
-int bytes_to_int(
-        char bytes[4]);
+/* Convert string to vector of uint32_t
+ * @param in_string: string of comma separated values
+ * @param delimiter: char to split by
+ * @return compressed_arr: uint32_t array
+ */
+uint32_t *convert_string_to_vector_uint32(string in_string, char delimiter);
 
-char * int_to_bytes(
-        int value);
+/*
+ * Convert bytes to int
+ * @param line: bytes
+ * @return value: int value
+ */
+int bytes_to_int(char bytes[4]);
 
-map<int, vector<uint32_t>> get_chrm_block_bp_ends(
-        string map_file);
+/*
+ * Convert int to bytes
+ * @param value: int
+ * @return bytes: char *
+ */
+char *int_to_bytes(int value);
 
-vector<int> get_block_sizes(
-        vector<vector<vector<string>>> all_blocks);
+/*
+ * Function to read a map file and find bp end of blocks
+ * @param map_file: string
+ * @return map of chrm: <bp, bp, bp...>
+ */
+map<int, vector<uint32_t>> get_chrm_block_bp_ends(string map_file);
+
+/*
+ * Function to get the sizes of blocks when using a map file
+ * @param all_blocks: vector of blocks
+ * @return vector of block sizes
+ */
+vector<int> get_block_sizes(vector<vector<vector<string>>> all_blocks);
+
+/*
+ * Split string by delimiter
+ * @param str: string
+ * @param delimiter: char to split by
+ * @return vector of strings
+ */
+vector<string> split_string(string str, char delimiter);
--- src/utils.cpp
+++ src/utils.cpp
@@ -1,27 +1,30 @@
-#include <iostream>
-#include <string>
-#include <vector>
 #include <fstream>
+#include <iostream>
 #include <map>
 #include <sstream>
+#include <string>
+#include <vector>
 
 #include "utils.h"
 
 using namespace std;
 
-char * int_to_bytes(
-        int value);
+char *int_to_bytes(int value);
 
-vector<string> split_string(
-        string str, char delimiter);
+vector<string> split_string(string str, char delimiter);
 
-/*
- * Read config file
- * @param config_file
- * @return vector of config options
- */
-map<string, string> read_config_file(
-        string config_file) {
+template <typename T>
+bool in_array(const T &value, const std::vector<T> &array) {
+    return std::find(array.begin(), array.end(), value) != array.end();
+}
+
+template <typename T, size_t N>
+bool in_array(const T &value, const T (&array)[N]) {
+    return std::find(std::begin(array), std::end(array), value) !=
+           std::end(array);
+}
+
+map<string, string> read_config_file(string config_file) {
     map<string, string> config_options;
 
     // check if file exists
@@ -35,7 +38,8 @@ map<string, string> read_config_file(
     string option_item;
     while (getline(config_stream, option_name)) {
         // remove ":" from option name
-        option_name.erase(remove(option_name.begin(), option_name.end(), ':'), option_name.end());
+        option_name.erase(remove(option_name.begin(), option_name.end(), ':'),
+                          option_name.end());
         getline(config_stream, option_item);
         // remove leading whitespace from option item
         option_item.erase(0, option_item.find_first_not_of(' '));
@@ -45,13 +49,7 @@ map<string, string> read_config_file(
     return config_options;
 }
 
-/*
- * Add default config options if not present
- * @param config_options
- * @return void
- */
-void add_default_config_options(
-        map<string, string> &config_options) {
+void add_default_config_options(map<string, string> &config_options) {
     // add default config options if option is empty
     if (config_options["block_size"].empty()) {
         config_options["block_size"] = "20";
@@ -63,16 +61,10 @@ void add_default_config_options(
     }
 }
 
-/*
- * Get index value of a string in a vector
- * @param vec: vector in which to look
- * @param str: string to search for
- * @return index: index value of string in vector
- */
-int get_index(vector<string> vec, string str){
+int get_index(vector<string> vec, string str) {
     int idx = -1;
-    for (int i = 0; i < vec.size(); i++){
-        if (vec[i] == str){
+    for (int i = 0; i < vec.size(); i++) {
+        if (vec[i] == str) {
             idx = i;
             break;
         }
@@ -80,18 +72,11 @@ int get_index(vector<string> vec, string str){
     return idx;
 }
 
-/*
- * Function to get the column types of a file
- * @param line: string
- * @param delimiter: char to split by
- * @return column_types_str: string of comma separated data types
- */
-string get_data_types(
-        string line,
-        char delimiter) {
+string get_data_types(string line, char delimiter) {
 
     string column_types_str;
-    // split line by delimiter find data type and store in comma separated string
+    // split line by delimiter find data type and store in comma separated
+    // string
     stringstream ss(line);
     string item;
     while (getline(ss, item, delimiter)) {
@@ -106,8 +91,7 @@ string get_data_types(
             try {
                 stoi(item);
                 column_types_str += "int,";
-            }
-            catch (invalid_argument &e) {
+            } catch (invalid_argument &e) {
                 column_types_str += "string,";
             }
         }
@@ -117,50 +101,34 @@ string get_data_types(
     return column_types_str;
 }
 
-/*
- * Function to get the delimiter of a file
- * @param line: string of first line of file
- * @return delimiter: char to split by
- */
-char get_delimiter(
-        string line){
+char get_delimiter(string line) {
     char delimiter;
     // determine delimiter
-    if(line.find('\t') != string::npos){
+    if (line.find('\t') != string::npos) {
         delimiter = '\t';
-    } else if(line.find(',') != string::npos){
+    } else if (line.find(',') != string::npos) {
         delimiter = ',';
-    } else if(line.find(' ') != string::npos){
+    } else if (line.find(' ') != string::npos) {
         delimiter = ' ';
     } else {
         cout << "Error: delimiter not found.\n"
-                " Please use files with delimiters as tab, comma, or space." << endl;
+                " Please use files with delimiters as tab, comma, or space."
+             << endl;
         exit(1);
     }
     return delimiter;
 }
 
-/*
- * Convert vector string to string
- * @param vec: vector of strings
- * @return str: string of comma separated values
- */
-string convert_vector_str_to_string(vector<string> vec){
+string convert_vector_str_to_string(vector<string> vec) {
     string str = "";
-    for (int i = 0; i < vec.size(); i++){
+    for (int i = 0; i < vec.size(); i++) {
         str += vec[i] + ",";
     }
     str.pop_back();
     return str;
 }
 
-/*
- * Convert vector of ints to string
- * @param vec: vector of integers
- * @return str: string of comma separated values
- */
-string convert_vector_int_to_string(
-        vector<int> vec){
+string convert_vector_int_to_string(vector<int> vec) {
     string str;
     for (int i = 0; i < vec.size(); i++) {
         str += to_string(vec[i]) + ",";
@@ -169,131 +137,97 @@ string convert_vector_int_to_string(
     return str;
 }
 
-string convert_vector_uint32_to_string(
-        uint32_t * compressed_arr,
-        size_t compressed_size){
+string convert_vector_uint32_to_string(uint32_t *compressed_arr,
+                                       size_t compressed_size) {
     string compressed_string;
-    for (int i = 0; i < compressed_size; i++){
+    for (int i = 0; i < compressed_size; i++) {
         compressed_string += to_string(compressed_arr[i]) + ",";
     }
     compressed_string.pop_back();
     return compressed_string;
 }
 
-/*
- * Convert string to vector
- * @param str: string of comma separated values
- * @return vector of strings
- */
-vector<string> convert_string_to_vector_string(string str){
+vector<string> convert_string_to_vector_string(string str) {
     vector<string> vec;
     istringstream line_stream(str);
     string column_value;
     while (getline(line_stream, column_value, ',')) {
         // remove newline character from column_value
-        column_value.erase(remove(column_value.begin(), column_value.end(), '\r'), column_value.end());
+        column_value.erase(
+            remove(column_value.begin(), column_value.end(), '\r'),
+            column_value.end());
         vec.push_back(column_value);
     }
     return vec;
 }
 
-uint32_t * convert_string_to_vector_uint32(string in_string, char delimiter){
+uint32_t *convert_string_to_vector_uint32(string in_string, char delimiter) {
     // split string by comma
     vector<string> vec = split_string(in_string, delimiter);
     // convert string to vector of uint32_t
-    uint32_t * compressed_arr = new uint32_t[vec.size()];
-    for (int i = 0; i < vec.size(); i++){
+    uint32_t *compressed_arr = new uint32_t[vec.size()];
+    for (int i = 0; i < vec.size(); i++) {
         compressed_arr[i] = stoul(vec[i]);
     }
     return compressed_arr;
 }
 
-
-/*
-* Convert string to vector
-* @param str: string of comma separated values
-* @return vector of int
-*/
-vector<uint32_t> convert_string_to_vector_unsignedlong(string str){
+vector<uint32_t> convert_string_to_vector_unsignedlong(string str) {
     vector<uint32_t> out_vec;
     istringstream line_stream(str);
     string column_value;
     while (getline(line_stream, column_value, ',')) {
         // remove newline character from column_value
-        column_value.erase(remove(column_value.begin(), column_value.end(), '\r'), column_value.end());
+        column_value.erase(
+            remove(column_value.begin(), column_value.end(), '\r'),
+            column_value.end());
         // convert data to uint32_t
         out_vec.push_back(stoul(column_value));
     }
     return out_vec;
 }
 
-/*
- * Convert bytes to int
- * @param line: bytes
- * @return value: int value
- */
-int bytes_to_int(char bytes[4]){
+int bytes_to_int(char bytes[4]) {
     int value = 0;
-    value += (unsigned char) bytes[0];
-    value += (unsigned char) bytes[1] << 8;
-    value += (unsigned char) bytes[2] << 16;
-    value += (unsigned char) bytes[3] << 24;
+    value += (unsigned char)bytes[0];
+    value += (unsigned char)bytes[1] << 8;
+    value += (unsigned char)bytes[2] << 16;
+    value += (unsigned char)bytes[3] << 24;
     return value;
 }
 
-/*
- * Convert int to bytes
- * @param value: int
- * @return bytes: char *
- */
-char * int_to_bytes(int value){
-    char * bytes = new char[4];
-    bytes[0] = (char) (value & 0xFF);
-    bytes[1] = (char) ((value >> 8) & 0xFF);
-    bytes[2] = (char) ((value >> 16) & 0xFF);
-    bytes[3] = (char) ((value >> 24) & 0xFF);
+char *int_to_bytes(int value) {
+    char *bytes = new char[4];
+    bytes[0] = (char)(value & 0xFF);
+    bytes[1] = (char)((value >> 8) & 0xFF);
+    bytes[2] = (char)((value >> 16) & 0xFF);
+    bytes[3] = (char)((value >> 24) & 0xFF);
     return bytes;
 }
 
-/*
- * Split string by delimiter
- * @param str: string
- * @param delimiter: char to split by
- * @return vector of strings
- */
-vector<string> split_string(string str, char delimiter){
+vector<string> split_string(string str, char delimiter) {
     vector<string> vec;
     istringstream line_stream(str);
     string column_value;
     while (getline(line_stream, column_value, delimiter)) {
         // remove newline character from column_value
-        column_value.erase(remove(column_value.begin(), column_value.end(), '\r'), column_value.end());
+        column_value.erase(
+            remove(column_value.begin(), column_value.end(), '\r'),
+            column_value.end());
         vec.push_back(column_value);
     }
     return vec;
 }
 
-/*
- * Function to convert a vector of strings to vector of integers
- * @param vec: vector of strings
- * @return vector of integers
- */
-vector<uint32_t> convert_vector_string_to_vector_int(
-        vector<string> vec){
+vector<uint32_t> convert_vector_string_to_vector_int(vector<string> vec) {
     vector<uint32_t> vec_int;
-    for (int i = 0; i < vec.size(); i++){
+    for (int i = 0; i < vec.size(); i++) {
         vec_int.push_back(stoi(vec[i]));
     }
     return vec_int;
 }
 
-/*
- * Function to read a map file and find bp end of blocks
- * @param map_file: string
- * @return map of chrm: <bp, bp, bp...>
- */
-map<int, vector<uint32_t>> get_chrm_block_bp_ends(
-        string map_file) {
+map<int, vector<uint32_t>> get_chrm_block_bp_ends(string map_file) {
     map<int, vector<uint32_t>> chrm_block_bp_ends;
     int BLOCK_CM_SIZE = 1;
     // open map file, exit if file does not exist
@@ -323,7 +257,9 @@ map<int, vector<uint32_t>> get_chrm_block_bp_ends(
         uint32_t bp = stoul(vec[3]);
         // if cm is greater than max_cm, add new block
         if (cm >= max_cm) {
-            if (cm == max_cm){ prev_bp = bp; }
+            if (cm == max_cm) {
+                prev_bp = bp;
+            }
             chrm_block_bp_ends[chrm].push_back(prev_bp);
             block_count++;
             max_cm = block_count * BLOCK_CM_SIZE + 1;
@@ -334,15 +270,9 @@ map<int, vector<uint32_t>> get_chrm_block_bp_ends(
     return chrm_block_bp_ends;
 }
 
-/*
- * Function to get the sizes of blocks when using a map file
- * @param all_blocks: vector of blocks
- * @return vector of block sizes
- */
-vector<int> get_block_sizes(
-        vector<vector<vector<string>>> all_blocks){
+vector<int> get_block_sizes(vector<vector<vector<string>>> all_blocks) {
     vector<int> block_sizes;
-    for (int i = 0; i < all_blocks.size(); i++){
+    for (int i = 0; i < all_blocks.size(); i++) {
         block_sizes.push_back(all_blocks[i][0].size());
     }
     return block_sizes;
