6c47d494ddbfff658877b19ec308b6e1e896d975
Simon Walker <simon@simonwalker.tech>
2024-06-25 21:44:30 -0600

    improved documentation for header.h/cpp

--- include/header.h
+++ include/header.h
@@ -1,25 +1,25 @@
 #ifndef GWAS_COMPRESS_CPP_HEADER_H
 #define GWAS_COMPRESS_CPP_HEADER_H
+#endif // GWAS_COMPRESS_CPP_HEADER_H
 
-#endif //GWAS_COMPRESS_CPP_HEADER_H
-
-#include <algorithm>
-#include <iostream>
 #include <string>
 #include <vector>
-#include <fstream>
-#include <sstream>
-#include <map>
 
 using namespace std;
 
-vector<string> parse_header_list(
-        vector<string> header_list,
-        string header_query);
+/*
+ * Parse header list
+ * @param header_list: vector<string>
+ * @param header_query: string
+ * @return header_query_list: vector<string>
+ */
+vector<string> parse_header_list(vector<string> header_list,
+                                 string header_query);
 
-string remove_zlib_header(
-        string compressed_string,
-        string zlib_header);
+/*
+ * Remove the common header from
+ * a string compressed with zlib
+ */
+string remove_zlib_header(string compressed_string, string zlib_header);
 
-string add_zlib_header(string compressed_string,
-                       string zlib_header);
+string add_zlib_header(string compressed_string, string zlib_header);
--- src/header.cpp
+++ src/header.cpp
@@ -1,22 +1,15 @@
 #include <iostream>
+#include <map>
 #include <string>
 #include <vector>
-#include <map>
 
-#include "header.h"
 #include "compress.h"
+#include "header.h"
 
 using namespace std;
 
-/*
- * Function to parse header list
- * @param header_list: vector<string>
- * @param header_query: string
- * @return header_query_list: vector<string>
- */
-vector<string> parse_header_list(
-        vector<string> header_list,
-        string header_query){
+vector<string> parse_header_list(vector<string> header_list,
+                                 string header_query) {
     vector<string> header_query_list;
     map<string, int> switch_case;
     switch_case["num columns"] = 0;
@@ -30,55 +23,59 @@ vector<string> parse_header_list(
 
     // switch statement for query element
     switch (switch_case[header_query]) {
-        case 0:
-            header_query_list.push_back(header_list[0]);
-            break;
-        case 1:
-            header_query_list.push_back(header_list[1]);
-            break;
-        case 2:
-            for (int i = 2; i < stoi(header_list[0])+2; i++){
+    case 0:
+        header_query_list.push_back(header_list[0]);
+        break;
+    case 1:
+        header_query_list.push_back(header_list[1]);
+        break;
+    case 2:
+        for (int i = 2; i < stoi(header_list[0]) + 2; i++) {
+            header_query_list.push_back(header_list[i]);
+        }
+        break;
+    case 3:
+        for (int i = stoi(header_list[0]) + 2;
+             i < stoi(header_list[0]) + 2 + stoi(header_list[1]); i++) {
+            header_query_list.push_back(header_list[i]);
+        }
+        break;
+    case 4:
+        for (int i = stoi(header_list[0]) + 2 + stoi(header_list[1]);
+             i < stoi(header_list[0]) + 2 + stoi(header_list[1]) * 2; i++) {
+            header_query_list.push_back(header_list[i]);
+        }
+        break;
+    case 5:
+        //            for (int i =
+        //            stoi(header_list[0])+2+stoi(header_list[1])*2; i <
+        //            stoi(header_list[0])+num_blocks+stoi(header_list[1])*2+num_blocks;
+        //            i++){
+        for (int i = stoi(header_list[0]) + 2 + stoi(header_list[1]) * 2;
+             i <
+             stoi(header_list[0]) + 2 + stoi(header_list[1]) * 2 + num_blocks;
+             i++) {
+            if (header_list[i] != "") {
                 header_query_list.push_back(header_list[i]);
+            } else {
+                break;
             }
-            break;
-        case 3:
-            for (int i = stoi(header_list[0])+2; i < stoi(header_list[0])+2+stoi(header_list[1]); i++){
-                header_query_list.push_back(header_list[i]);
-            }
-            break;
-        case 4:
-            for (int i = stoi(header_list[0])+2+stoi(header_list[1]); i < stoi(header_list[0])+2+stoi(header_list[1])*2; i++){
-                header_query_list.push_back(header_list[i]);
-            }
-            break;
-        case 5:
-//            for (int i = stoi(header_list[0])+2+stoi(header_list[1])*2; i < stoi(header_list[0])+num_blocks+stoi(header_list[1])*2+num_blocks; i++){
-            for (int i = stoi(header_list[0])+2+stoi(header_list[1])*2; i < stoi(header_list[0])+2+stoi(header_list[1])*2+num_blocks; i++){
-                if (header_list[i] != "") {
-                    header_query_list.push_back(header_list[i]);
-                }else{ break; }
-            }
-            break;
-        default:
-            cout << "Error: header_query not found." << endl;
-            exit(1);
+        }
+        break;
+    default:
+        cout << "Error: header_query not found." << endl;
+        exit(1);
     }
 
     return header_query_list;
 }
 
-/*
- * Function to remove the common header from
- * a string compressed with zlib
- */
-string remove_zlib_header(string compressed_string,
-                          string zlib_header){
+string remove_zlib_header(string compressed_string, string zlib_header) {
     string zlib_header_removed = compressed_string.substr(zlib_header.length());
     return zlib_header_removed;
 }
 
-string add_zlib_header(string compressed_string,
-                       string zlib_header){
+string add_zlib_header(string compressed_string, string zlib_header) {
     string zlib_header_added = zlib_header + compressed_string;
     return zlib_header_added;
 }
