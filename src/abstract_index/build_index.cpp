d3599bba63a3a0ee34d6ac5bd88438547da4ff2c
Simon Walker <simon@simonwalker.tech>
2024-08-05 23:43:31 -0600

    indexers::build_index -> build_index::build_index

---
+++ src/abstract_index/build_index.cpp
@@ -0,0 +1,80 @@
+#include "indexers.h"
+#include "utils.h"
+#include <set>
+#include <unordered_map>
+#include <vector>
+
+void Indexer::build_index(std::string inPath, std::string outPath) {
+    std::ifstream file(inPath);
+
+    if (!file.is_open()) {
+        throw std::runtime_error("Error opening file");
+    }
+
+    std::string lineStr;
+    if (!std::getline(file, lineStr)) {
+        throw std::runtime_error("Missing header line");
+    }
+
+    int blockSize = 3;
+    int queryCol = 9;
+
+    std::unordered_map<int, std::vector<int>> index;
+
+    // 1. Bin the distribution of data
+
+    int lineId = 0;
+    bool moreBlocks = true;
+    while (moreBlocks) {
+        int blockId = lineId / blockSize;
+        std::set<int> bins;
+
+        for (int i = 0; i < blockSize; i++) {
+            if (!std::getline(file, lineStr)) {
+                moreBlocks = false;
+                break;
+            }
+
+            lineId++;
+            auto rowVals = split_string(lineStr, '\t');
+            std::string queryVal = rowVals[queryCol];
+            int bin = value_to_bin(queryVal);
+            bins.insert(bin);
+        }
+
+        for (int bin : bins) {
+            index[bin].push_back(blockId);
+        }
+    }
+
+    // 2. Serialize the index map
+
+    std::ofstream indexFile(outPath);
+    std::unordered_map<int, int> binPositions;
+
+    for (auto &entry : index) {
+        binPositions[entry.first] = indexFile.tellp();
+
+        for (int i = 0; i < entry.second.size() - 1; i++) {
+            indexFile << entry.second[i] << " ";
+        }
+
+        indexFile << entry.second[entry.second.size() - 1] << std::endl;
+    }
+
+    // 3. Write the footer (map keys)
+
+    indexFile << std::endl;
+
+    int pos0 = indexFile.tellp();
+
+    for (auto &entry : binPositions) {
+        std::string key = bin_to_value(entry.first);
+        indexFile << key << " " << entry.second << std::endl;
+    }
+
+    int footerSize = (int)(indexFile.tellp()) - pos0;
+    indexFile << footerSize << std::endl;
+
+    indexFile.close();
+}
